b6a5f10f583afa160e1a63e6a4cb63321d57f219
Justin Albano <albanoj2@users.noreply.github.com>
2018-02-06 16:52:49 -0500

    Swapped conditionals for both GoalieStatistics methods

--- src/main/java/com/dzone/albanoj2/refactoring/hockey/GoalieStatistics.java
+++ src/main/java/com/dzone/albanoj2/refactoring/hockey/GoalieStatistics.java
@@ -10,27 +10,27 @@ public class GoalieStatistics {
 
     public double getGoalsAgainstAverage() {
 
-        if (!season.hasStarted()) {
-            return 0.0;
-        }
-        else {
+        if (season.hasStarted()) {
             int totalGoalsAgainst = season.getTotalGoalsAgainst();
             double totalMinutesPlayed = season.getTotalMinutesPlayed();
 
             return (totalGoalsAgainst / totalMinutesPlayed) * 60;
         }
+        else {
+            return 0.0;
+        }
     }
 
     public double getSavePercentage() {
 
-        if (!season.hasStarted()) {
-            return 0.0;
-        }
-        else {
+        if (season.hasStarted()) {
             int totalGoalsAgainst = season.getTotalGoalsAgainst();
             int totalSogAgainst = season.getTotalShotsOnGoalAgainst();
 
             return ((double) totalSogAgainst - totalGoalsAgainst) / totalSogAgainst;
+        }
+        else {
+            return 0.0;
         }
     }
 }
